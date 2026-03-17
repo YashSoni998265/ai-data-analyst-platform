ee2b84ba4717297be5908f4fa3bf65ccb69d06c8
Yash Soni <167873745+YashSoni998265@users.noreply.github.com>
2026-03-17 22:23:01 +0530

    Day 1: Backend Setup and PostgreSQL Connection

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const pool = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("AI Data Analyst Backend Running");
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("DB Error");
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
